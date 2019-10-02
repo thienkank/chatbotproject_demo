afe246669ed185948272fd2711db2c8dc579ceab
Thienkan Khetpratum <thienkan.k@gmail.com>
2019-10-02 12:13:14 +0700

    Change engine back to pylexto

--- app.py
+++ app.py
@@ -39,8 +39,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    # result=word_tokenize(event.message.text, engine='pylexto')
-    result=word_tokenize(event.message.text, engine='newmm')
+    result=word_tokenize(event.message.text, engine='pylexto')
     segment="|".join(str(i) for i in result)
     line_bot_api.reply_message(event.reply_token,TextSendMessage(text=segment))
 
