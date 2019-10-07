384608df819048e9aa9bd34e2ff537c48f80ebc5
Thienkan Khetpratum <thienkan.k@gmail.com>
2019-10-07 20:42:18 +0700

    Updated app.py for Dialogflow

--- app.py
+++ app.py
@@ -1,47 +1,28 @@
-from flask import Flask, request, abort, render_template
+from flask import Flask, make_response, request, jsonify, render_template
 from pythainlp import word_tokenize
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-)
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('wEAYlhnzhVzBVbSJnXhbbsOp/yccxr733nsKhZx3KX45MAa5lv7rdVuv+Qtju5w39DCU4HKiFqdJlEJdSEr///obW6spUhtbyPRS+KaTNF6gYIKH32ytWm21wsnM29630MMmhp1OCb9OI+tnPJaa0gdB04t89/1O/w1cDnyilFU=') #CHANNEL_ACCESS_TOKEN
-handler = WebhookHandler('972fe2410b1815a28ff6a68ac8f410b7') #CHANNEL_SECRET
+def tokenize(sentence):
+    words=word_tokenize(sentence, engine='pylexto')
+    tokens="|".join(str(i) for i in words)
+    return tokens
+
+def response():# definition of the results function
+    req = request.get_json(force=True)
+    sentence = req.get('queryResult')
+    result = {} # an empty dictionary
+    result["fulfillmentText"] = tokenize(sentence)
+    result = jsonify(result)
+    return make_response(result)# return the result json
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/callback", methods=['GET','POST'])
-def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
-
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        print("Invalid signature. Please check your channel access token/channel secret.")
-        abort(400)
-
-    return 'OK'
-
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    result=word_tokenize(event.message.text, engine='pylexto')
-    segment="|".join(str(i) for i in result)
-    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=segment))
+@app.route("/webhook", methods=['POST'])
+def webhook():
+    return response()
 
 if __name__ == "__main__":
     app.run()
