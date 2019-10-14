b25e8dce13d0071301a6ab912333424284f37456
Thienkan Khetpratum <thienkan.k@gmail.com>
2019-10-14 21:20:42 +0700

    Back to OG

--- app.py
+++ app.py
@@ -3,27 +3,10 @@ from pythainlp import word_tokenize
 
 app = Flask(__name__)
 
-# def tokenize(sentence):
-#     words=word_tokenize(sentence, engine='pylexto')
-#     tokens="|".join(str(i) for i in words)
-#     return tokens
-
-# def response():# definition of the results function
-#     req = request.get_json(force=True)
-#     sentence = req.get('queryResult').get('queryText')
-#     result = {} # an empty dictionary
-#     result["fulfillmentText"] = tokenize(sentence)
-#     result = jsonify(result)
-#     return make_response(result)# return the result json
-
-oldm = 'TEST'
-
 def tokenize(sentence):
-    # words=word_tokenize(sentence, engine='pylexto')
-    global oldm
-    old=oldm
-    oldm=sentence
-    return old
+    words=word_tokenize(sentence, engine='pylexto')
+    tokens="|".join(str(i) for i in words)
+    return tokens
 
 def response():# definition of the results function
     req = request.get_json(force=True)
