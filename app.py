8028061b686083257688394c099eb93bbdd0460f
Thienkan Khetpratum <thienkan.k@gmail.com>
2019-10-30 03:14:04 +0700

    test external api again

--- app.py
+++ app.py
@@ -3,18 +3,21 @@ from pythainlp import word_tokenize
 
 app = Flask(__name__)
 
-def tokenize(sentence):
-    words=word_tokenize(sentence, engine='pylexto')
-    tokens="|".join(str(i) for i in words)
-    return tokens
+# def tokenize(sentence):
+#     words=word_tokenize(sentence, engine='pylexto')
+#     tokens="|".join(str(i) for i in words)
+#     return tokens
 
-def response():# definition of the results function
-    req = request.get_json(force=True)
-    sentence = req.get('queryResult').get('queryText')
-    result = {} # an empty dictionary
-    result["fulfillmentText"] = tokenize(sentence)
-    result = jsonify(result)
-    return make_response(result)# return the result json
+# def response():# definition of the results function
+#     req = request.get_json(force=True)
+#     sentence = req.get('queryResult').get('queryText')
+#     result = {} # an empty dictionary
+#     result["fulfillmentText"] = tokenize(sentence)
+#     result = jsonify(result)
+#     return make_response(result)# return the result json
+
+def response():
+    return "Hello from Heroku"
 
 @app.route("/")
 def index():
