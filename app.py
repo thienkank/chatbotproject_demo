9460d053ce5918fad63ab9e83cebd360ab0df1af
Thienkan Khetpratum <thienkan.k@gmail.com>
2019-11-06 04:12:55 +0700

    Convert string to numeric

--- app.py
+++ app.py
@@ -20,13 +20,41 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/webhook", methods=['GET','POST'])
+@app.route("/webhook", methods=['POST'])
 def webhook():
     # return response()
     req = request.get_json(force=True)
-    sentence = req.get('queryResult').get('parameters').get('Event')
+    event = req.get('queryResult').get('parameters').get('Event')
+    sex = req.get('queryResult').get('parameters').get('Sex')
+    relationship = req.get('queryResult').get('parameters').get('Relation')
+    age = req.get('queryResult').get('parameters').get('Age')
+    #price = req.get('queryResult').get('parameters').get('Price')
+    if event == 'วาเลนไทน์':
+        event = 0
+    elif event == 'ปีใหม่':
+        event = 1
+    else :
+        event = 2
+    if sex == 'ชาย':
+        sex = 0
+    elif sex == 'หญิง':
+        sex = 1
+    else:
+        sex = 0
+    if relationship == 'ครอบครัว':
+        relationship = 0
+    elif relationship == 'เพื่อน':
+        relationship = 1
+    else:
+        relationship = 2
+    if age == 'วัยรุ่น':
+        age = 0
+    elif age == 'ผู้ใหญ่':
+        age = 1
+    else:
+        age = 2
     result = {} # an empty dictionary
-    result["fulfillmentText"] = sentence
+    result["fulfillmentText"] = event+" "+sex+" "+relationship+" "+age
     result = jsonify(result)
     return make_response(result)# return the result json
 if __name__ == "__main__":
