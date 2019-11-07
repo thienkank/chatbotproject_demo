939a4afffa6737dd75992f2d5ffa9c6f83e580e9
Thienkan Khetpratum <thienkan.k@gmail.com>
2019-11-07 18:24:06 +0700

    added model and new requirement

--- app.py
+++ app.py
@@ -29,30 +29,30 @@ def webhook():
     relationship = req.get('queryResult').get('parameters').get('Relationship')
     age = req.get('queryResult').get('parameters').get('Age')
     price = req.get('queryResult').get('parameters').get('Price')
-    # if event == 'วาเลนไทน์':
-    #     event = 0
-    # elif event == 'ปีใหม่':
-    #     event = 1
-    # else :
-    #     event = 2
-    # if sex == 'ชาย':
-    #     sex = 0
-    # elif sex == 'หญิง':
-    #     sex = 1
-    # else:
-    #     sex = 0
-    # if relationship == 'ครอบครัว':
-    #     relationship = 0
-    # elif relationship == 'เพื่อน':
-    #     relationship = 1
-    # else:
-    #     relationship = 2
-    # if age == 'วัยรุ่น':
-    #     age = 0
-    # elif age == 'ผู้ใหญ่':
-    #     age = 1
-    # else:
-    #     age = 2
+    if event == 'วาเลนไทน์':
+        event = 0
+    elif event == 'ปีใหม่':
+        event = 1
+    else :
+        event = 2
+    if sex == 'ชาย':
+        sex = 0
+    elif sex == 'หญิง':
+        sex = 1
+    else:
+        sex = 0
+    if relationship == 'ครอบครัว':
+        relationship = 0
+    elif relationship == 'เพื่อน':
+        relationship = 1
+    else:
+        relationship = 2
+    if age == 'วัยรุ่น':
+        age = 0
+    elif age == 'ผู้ใหญ่':
+        age = 1
+    else:
+        age = 2
     result = {} # an empty dictionary
     result["fulfillmentText"] = event+" "+ sex+" "+relationship+" "+age+" "+price
     result = jsonify(result)
